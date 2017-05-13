c06a8f899ad69e74e056fd95e30d0c40f8e4a767
4M1LK4R <amilkar.dominguez@gmail.com>
2017-05-13 17:10:36 -0400

    Servicios
    Intento de mejorar servicios

--- plugins/materialize/js/init.js
+++ plugins/materialize/js/init.js
@@ -18,6 +18,19 @@ $(document).ready(function () {
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
+  $('input.autocomplete').autocomplete({
+    data: {
+      "101010": null,
+      "202020": null,
+      "303030":  null,
+      "404040": null
+    },
+    limit: 20, // The max amount of results that can be shown at once. Default: Infinity.
+    onAutocomplete: function (val) {
+      // Callback function when value is autcompleted.
+    },
+    minLength: 1, // The minimum length of the input for the autocomplete to start. Default: 1.
+  });
 });
 
 
